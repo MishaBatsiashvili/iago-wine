e42ed1895a379df77d4dba0ab1d39362bbcfa5b2
misha2016 <mishabatsiashvili@yahoo.com>
2020-08-19 20:40:29 +0400

    added env handling packages and updated custom axios instance and CartItems.js

--- src/api/customAxios.js
+++ src/api/customAxios.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import qs from 'querystring';
 
+console.log(process.env.SECRET_CODE);
 const customAxios = axios.create({
-   baseURL: 'https://iago.ge/api/api.php',
+   baseURL: process.env.REACT_APP_APIURL,
    withCredentials: true,
    headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
    },
 });
+
+console.log(process.env);
+// debugger;
 
 customAxios.interceptors.request.use(function (config) {
    console.log(config);
--- src/components/Navbar/Cart/CartItems.js
+++ src/components/Navbar/Cart/CartItems.js
@@ -10,70 +10,65 @@ class CartItems extends React.Component {
 
    componentDidMount() {
       this.setState({
-         products: this.props.cartData.products.map(prod => {
-            return {
-               id: prod.id,
-               amount: prod.amount,
-            }
-         }),
+         products: this.duplicateCartData(),
       });
    }
 
    componentDidUpdate(prevProps, prevState, snapshot) {
       if(this.props.cartData !== prevProps.cartData){
-         const newState = this.props.cartData.products.map(prod => {
-            return {
-               id: prod.id,
-               amount: prod.amount,
-            }
-         });
          this.setState({
-            products: newState,
+            products: this.duplicateCartData(),
          });
       }
    }
 
+   duplicateCartData = () => {
+      return this.props.cartData.products.map(prod => {
+         return {
+            ...prod
+         }
+      });
+   }
+
+   getStateWithChangedAmount = (id, newAmount, state) => {
+      return state.products.map(el => {
+         if(el.id === id && newAmount >= 1){
+            return {
+               ...el,
+               amount: newAmount,
+            }
+         }
+         return el;
+      });
+   }
+
    onAmountChanged = (id, newAmount) => {
       this.setState((state) => {
-         const newState = state.products.map(el => {
-            if(el.id === id && newAmount >= 1){
-               return {
-                  ...el,
-                  amount: newAmount,
-               }
-            }
-            return el;
-         });
-         // debugger;
-
-         return {products: newState}
+         return {products: this.getStateWithChangedAmount(id, newAmount, state)}
       });
    }
 
    render() {
 
-      if(this.state.products.length === 0) return <div>Loading...</div>;
-
       const cartItemsJSX = () => {
-         // debugger;
-         return this.props.cartData.products.map((prod, i) => (
-            <tr className={s.itemwrp}>
-               <td className={s.itemTitleWrp}>
-                  <Link to={'#'} className={s.itemName}>{prod[`name_${this.props.lang}`]}</Link>
-               </td>
-               <td className={s.itemPrice}>{prod.price} GEL</td>
-               <td className={s.itemActions}>
-                  <input
-                     type="number"
-                     className={s.amntInput}
-                     value={this.state.products[i].amount}
-                     onChange={(e) => this.onAmountChanged(prod.id, e.target.value)}
-                     onBlur={(e) => this.props.changeAmnt(prod.id, e.target.value, this.props.cartData.id)}
-                  />
-                  <i className="fas fa-trash-alt"></i>
-               </td>
-            </tr>
-         ))
+         return this.state.products.map((prod) => (
+               <tr className={s.itemwrp}>
+                  <td className={s.itemTitleWrp}>
+                     <Link to={'#'} className={s.itemName}>{prod[`name_${this.props.lang}`]}</Link>
+                  </td>
+                  <td className={s.itemPrice}>{prod.price} GEL</td>
+                  <td className={s.itemActions}>
+                     <input
+                        type="number"
+                        className={s.amntInput}
+                        value={prod.amount}
+                        onChange={(e) => this.onAmountChanged(prod.id, e.target.value)}
+                        onBlur={(e) => this.props.changeAmnt(prod.id, e.target.value, this.props.cartData.id)}
+                     />
+                     <i className="fas fa-trash-alt"></i>
+                  </td>
+               </tr>
+            ))
       }
 
       return (<table className={`w-100`}>
